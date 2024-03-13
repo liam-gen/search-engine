31a090f62ac9feeddcb625064eb42321f72476ca
liamgen.js <liamgen.js@proton.me>
2024-03-13 17:43:58 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,23 +8,11 @@ app.set('json spaces', 4)
 
 app.get('/api/search/:search', (req, res) => {
   let database = JSON.parse(fs.readFileSync("database.json", "utf8"));
-
-    /*const fuse = new Fuse(database, {
-        isCaseSensitive: true,
-        includeScore: true,
-        shouldSort: true,
-        includeMatches: true,
-        findAllMatches: true,
-        threshold: 0.3,
-        useExtendedSearch: true,
-        keys: ['url', 'tab_title', 'meta_description', 'meta_author', 'meta_title', 'meta_keywords']
-    })*/
-
     const MiniSearch = require('minisearch');
-
+    
     let miniSearch = new MiniSearch({
-        fields: ['url', 'tab_title', 'meta_description', 'meta_keywords', 'meta_author', 'meta_title'], // fields to index for full-text search
-        storeFields: ['url', 'tab_title', 'meta_description', 'meta_keywords', 'meta_author', 'meta_title', 'meta_icon', 'images', 'sitemaps'] // fields to return with search results
+        fields: ['url', 'tab_title', 'meta_description', 'meta_keywords', 'meta_author', 'meta_title', 'meta_title', "meta_og_url", "meta_og_site_name", "meta_og_title"], // fields to index for full-text search
+        storeFields: ['url', 'tab_title', 'meta_description', 'meta_keywords', 'meta_author', 'meta_title', 'meta_icon', 'images', "meta_og_url", "meta_og_site_name", "meta_og_image", "meta_og_type", "meta_og_title", "videos", "sitemaps"] // fields to return with search results
     })
 
     let num = 0;
